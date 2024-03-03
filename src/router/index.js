40501562044cbc6c07a1cd5f0e94d1eb2e8cf90d
HuntEvening <79421624+HuntEvening@users.noreply.github.com>
2024-03-03 14:13:56 +0100

    added base files

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomePage from '../views/Home.vue'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomePage
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  components: {},
+  data() {
+    return {
+   
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/base.scss';
+
+</style>
